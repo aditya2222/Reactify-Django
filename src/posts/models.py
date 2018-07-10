d3dfc76741ecb32a527f9683c7b885d1038168c9
adityasingh222247@gmail.com <adityasingh222247@gmail.com>
2018-07-10 11:03:27 +0530

    Final Commit

--- src/posts/models.py
+++ src/posts/models.py
@@ -2,13 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save
 
-
 from .utils import unique_slug_generator
 
 
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+                             on_delete=models.CASCADE,blank=True,null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
